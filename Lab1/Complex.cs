db3b1f5ee30a5d77c33e4d1e9a2eb6357b03fb79
Timur <timengalytchev@mail.ru>
2024-02-12 13:28:59 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Complex.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    internal class Complex
+    {
+        public double Real {  get; set; }
+        public double Imag { get; set; }
+
+        public void Add(Complex x)
+        {
+            Real += x.Real;
+            Imag += x.Imag;
+        }
+
+        public void Subtract(Complex x)
+        {
+            Real -= x.Real;
+            Imag -= x.Imag;
+        }
+
+        public void Multiply(Complex x)
+        {
+            Real *= x.Real;
+            Imag *= x.Imag;
+        }
+
+        public void Divide(Complex x)
+        {
+            Real /= x.Real;
+            Imag /= x.Imag;
+        }
+    }
+}
---
+++ Lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("LAb 1");
+            Console.WriteLine("MAde Timur Engalytchev");
+
+            Complex a = new() { Real = 3, Imag = 0.5 };
+            Complex b = new() { Real = 2, Imag = 0.8 };
+
+            a.Add(b);
+
+            b.Subtract(a);
+
+            Console.WriteLine("a = {0} + {1}i", a.Real, a.Imag);
+            Console.WriteLine("b = {0} + {1}i", b.Real, b.Imag);
+        }
+    }
+}
